7bbc68dbde51dad7885a9e8fc5f33c2409d263e4
A. Emre Ünal <aemreunal@gmail.com>
2014-04-29 12:53:50 +0300

    Add resources link.

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,47 +1,49 @@
 package client;
 
-import javax.swing.*;
-
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-class Main extends JFrame
-{
-JPanel p1,p2;
-Dimension d;
 
-    public Main()
-    {
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class Main extends JFrame {
+    JPanel p1, p2;
+    Dimension d;
+    
+    public Main() {
         createAndShowGUI();
     }
     
-    private void createAndShowGUI()
-    {
+    private void createAndShowGUI() {
         setTitle("Killing Frenzy");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        //setLayout(new FlowLayout());
-        p1=new JPanel();
-        p1.setPreferredSize(new Dimension(500,500));
+        // setLayout(new FlowLayout());
+        p1 = new JPanel();
+        p1.setPreferredSize(new Dimension(500, 500));
         p1.setBackground(Color.GRAY);
-        p1.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY,5,true));
+        p1.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 5, true));
         
-        JButton okButton = new JButton("JOIN THE MADNESS"); 
+        JButton okButton = new JButton("JOIN THE MADNESS");
         p1.add(okButton);
         add(p1);
         
         okButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-            	
-            }          
-         });
+                
+            }
+        });
         
-        setSize(500,500);
+        setSize(500, 500);
         setVisible(true);
-        pack();      
+        pack();
     }
     
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         new Main();
     }
 }
