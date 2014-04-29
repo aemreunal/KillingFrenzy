863514c2267a76b649a1cad0602d82dff6661693
A. Emre Ünal <aemreunal@gmail.com>
2014-04-29 17:43:18 +0300

    Update package structure, create new classes.

---
+++ src/clientSide/attributes/gun/Gun.java
@@ -0,0 +1,12 @@
+package clientSide.attributes.gun;
+
+/*
+ * This code belongs to:
+ * Ahmet Emre Unal
+ * S001974
+ * emre.unal@ozu.edu.tr
+ */
+
+public class Gun {
+    
+}
---
+++ src/clientSide/attributes/player/Player.java
@@ -0,0 +1,14 @@
+package clientSide.attributes.player;
+
+/*
+ * This code belongs to:
+ * Ahmet Emre Unal
+ * S001974
+ * emre.unal@ozu.edu.tr
+ */
+
+public class Player {
+    public Player() {
+        
+    }
+}
