006d9d5c95f384f0559f31b41478faad81e93234
A. Emre Ünal <aemreunal@gmail.com>
2014-05-07 18:29:03 +0300

    Add Entity object to client side.

---
+++ src/clientSide/attributes/Entity.java
@@ -0,0 +1,35 @@
+package clientSide.attributes;
+
+/**
+ * Created by Eren Sezener
+ */
+public abstract class Entity {
+    private int id;
+    private PhysicalAttributes physicalAttributes;
+
+    public Entity() {
+        this(0, 0);
+    }
+
+    public Entity(float height, float width) {
+        this(0, 0, height, width);
+    }
+
+    public Entity(float x, float y, float height, float width) {
+        this(0, x, y, height, width);
+    }
+
+    public Entity(int id, float x, float y, float height, float width) {
+        this.physicalAttributes = new PhysicalAttributes(x, y, height, width);
+        this.id = 0;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/clientSide/attributes/world/World.java
@@ -0,0 +1,35 @@
+package clientSide.attributes.world;
+
+import clientSide.attributes.Entity;
+
+import java.util.TreeMap;
+
+/**
+ * Created by Eren Sezener
+ */
+public class World {
+    private TreeMap<Integer, Entity> idToEntityMap;
+    private static World world;
+
+    public static World getInstance() {
+        if (world == null) {
+            world = new World();
+        }
+        return world;
+    }
+
+    private World() {
+        idToEntityMap = new TreeMap<>();
+    }
+
+    public void addEntity(Entity entity, int entityId) {
+        entity.setId(entityId);
+        idToEntityMap.put(entity.getId(), entity);
+    }
+
+
+    public Entity voidGetEntity(int id) {
+        return idToEntityMap.get(id);
+    }
+
+}
