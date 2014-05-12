a406ab79463fd57a7f243ebd115effc006bea524
Eren Sezener <erensezener@gmail.com>
2014-05-12 22:10:50 +0300

    Add some tests

---
+++ tests/serverSide/gamemechanics/WorldTest.java
@@ -0,0 +1,38 @@
+package serverSide.gamemechanics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WorldTest {
+    World world = World.getInstance();
+
+    @Test
+    public void testGetInstance() throws Exception {
+        //Can only create one instance
+        World world2 = World.getInstance();
+        assertSame(world, world2);
+    }
+
+    @Test
+    public void testAddEntity() throws Exception {
+        world.addEntity(new Bullet());
+        assertEquals(world.idToEntityMap.size(), 1);
+    }
+
+    @Test
+    public void testRemoveEntity() throws Exception {
+        Bullet bullet = new Bullet();
+        world.addEntity(bullet);
+        assertEquals(world.idToEntityMap.size(), 1);
+        world.removeEntity(bullet);
+        assertEquals(world.idToEntityMap.size(), 0);
+    }
+
+    @Test
+    public void testGetEntity() throws Exception {
+        Bullet bullet = new Bullet();
+        world.addEntity(bullet);
+        assertSame(bullet, world.getEntity(bullet.getId()));
+    }
+}
