c302bfc7d1b9fecabe1cc9b4e908e0df94e072d9
Erdi Gültekin <erdi.gultekin@ozu.edu.tr>
2014-05-15 19:08:18 +0300

    Map details added for collision detection.

--- src/serverSide/gamemechanics/Wall.java
+++ src/serverSide/gamemechanics/Wall.java
@@ -4,11 +4,20 @@ package serverSide.gamemechanics;
  * Created by Eren Sezener
  */
 public class Wall extends Entity implements Collidable {
-    private static final int WIDTH = 5;
-    private static final int HEIGHT = 5;
-
-    @Override
-    public void update() {
-
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 10;
+    
+    public Wall(int x, int y, boolean isVertical, int size) {
+    	if(isVertical){
+    		this.physicalAttributes = new PhysicalAttributes(x, y, size*HEIGHT, WIDTH);
+    	}else{
+    		this.physicalAttributes = new PhysicalAttributes(x, y, HEIGHT,size*WIDTH);
+    	}
     }
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
 }
